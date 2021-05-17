9116857cfc3b4d782fe79e48cffbc7c4fa1c5470
AnupamaBalasooriya_98 <58579939+AnupamaBalasooriya98@users.noreply.github.com>
2021-05-17 12:27:15 +0530

    Create supplier account service project

---
+++ supplier-account/src/main/java/com/sliit/mtit/microservices/supplieraccount/SupplierAccountApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.mtit.microservices.supplieraccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplierAccountApplication.class, args);
+	}
+
+}
---
+++ supplier-account/src/test/java/com/sliit/mtit/microservices/supplieraccount/SupplierAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.mtit.microservices.supplieraccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
